20adb35e8a0f27160d80303770cae35095b78ccb
vitalspace <myglobalsounds@gmail.com>
2022-03-28 20:58:50 -0600

    Update live.js

--- live.js
+++ live.js
@@ -2,28 +2,27 @@
 
 const ffmpeg = require('fluent-ffmpeg');
 
-const youtube = '';
-const stramkey = ''; // change this for you own stramkey 
-const video = `` // we will bring the direct link of this video.
+const youtube = 'rtmp://ovsu.mycdn.me/input/';
+const stramkey = '4004618971925_2406873369109_p243ia67we'; // change this for you own stramkey 
+const url = `http://hammertv.net:25461/hadrien/hadrien548/16589`; // we will bring the direct link of this video.
 
 const createStream = async () => {
 
-  await ffmpeg()
-
+  try {
+    await ffmpeg()
     .addOption("-i", url)
     .addOption("-c", "copy")
     .addOption("-f", "flv")
     .on("start", (commandLine) => {
       console.log("Query : ", commandLine);
     })
-    .on("error", async (err) => {
-      if (err) {
-        await createStream();
-        console.log("Error: " + err.message);
-      }
-    })
     .output(youtube + stramkey)
     .run();
+  } catch (error) {
+    await createStream();
+  }
+
 }
 createStream();
+
 
